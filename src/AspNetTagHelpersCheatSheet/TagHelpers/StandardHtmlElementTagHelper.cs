43d6dc8cca4b0cbfd9489e9e2173503fba75b933
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-01-18 21:03:19 +0100

    Created tag helpers targetting p elements

--- src/AspNetTagHelpersCheatSheet/TagHelpers/StandardHtmlElementTagHelper.cs
+++ src/AspNetTagHelpersCheatSheet/TagHelpers/StandardHtmlElementTagHelper.cs
@@ -1,17 +1,41 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
-
+using System.Threading.Tasks;
 
 namespace AspNetTagHelpersCheatSheet.TagHelpers
 {
     [HtmlTargetElement("p")]
     public class StandardHtmlElementTagHelper : TagHelper
     {
-        public override void Process(TagHelperContext context, TagHelperOutput output)
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            /*
-             * Erases the content of the targeted element
-             */
-            output.Content.SetHtmlContent("Erased !");
+            TagHelperContent content = null;
+            if (output.Content.IsModified)
+            {
+                content = output.Content;
+            }
+            else
+            {
+                content = await output.GetChildContentAsync();
+            }
+            output.Content.SetHtmlContent("Prepend 1 -> " + content.GetContent());
+        }
+    }
+
+    [HtmlTargetElement("p")]
+    public class OtherStandardHtmlElementTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            TagHelperContent content = null;
+            if (output.Content.IsModified)
+            {
+                content = output.Content;
+            }
+            else
+            {
+                content = await output.GetChildContentAsync();
+            }
+            output.Content.SetHtmlContent("Other Prepend -> " + content.GetContent());
         }
     }
 }
