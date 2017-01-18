a036f27167963f411a7bfb612c4ccaa9d9ced959
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-01-18 17:52:27 +0100

    Created the CustomNamedTagHelper

---
+++ src/AspNetTagHelpersCheatSheet/TagHelpers/CustomNamedTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetTagHelpersCheatSheet.TagHelpers
+{
+    [HtmlTargetElement("arbitrary-name")]
+    public class CustomNamedTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "p";
+            output.Content.SetHtmlContent("Arbitrarly named!");
+        }
+    }
+}
