422ce01a0b8185d03c91d54392c70077af4061e5
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-01-18 18:51:09 +0100

    Created StandardHtmlElementTagHelper

---
+++ src/AspNetTagHelpersCheatSheet/TagHelpers/StandardHtmlElementTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+
+namespace AspNetTagHelpersCheatSheet.TagHelpers
+{
+    [HtmlTargetElement("p")]
+    public class StandardHtmlElementTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            /*
+             * Erases the content of the targeted element
+             */
+            output.Content.SetHtmlContent("Erased !");
+        }
+    }
+}
