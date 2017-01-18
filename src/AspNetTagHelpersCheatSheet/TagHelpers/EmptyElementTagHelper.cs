40183eeec6dd9234c6d99a85d0d55ce72632555d
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-01-18 18:22:22 +0100

    Created EmptyElementTagHelper

---
+++ src/AspNetTagHelpersCheatSheet/TagHelpers/EmptyElementTagHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetTagHelpersCheatSheet.TagHelpers
+{
+    [HtmlTargetElement("empty-element", TagStructure = TagStructure.WithoutEndTag)]
+    public class EmptyElementTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "hr";
+        }
+    }
+}
