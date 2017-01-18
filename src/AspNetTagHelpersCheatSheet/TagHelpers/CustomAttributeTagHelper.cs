01d2d02097cab02a0cd4b2e9ec1a31ed8ab4bcf8
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-01-18 18:39:23 +0100

    Created CustomAttributeTagHelper

---
+++ src/AspNetTagHelpersCheatSheet/TagHelpers/CustomAttributeTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace AspNetTagHelpersCheatSheet.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "hello-prefix")]
+    public class CustomAttributeTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            /*
+             * Remove the custom attribute since it will be useless
+             * after rendering
+             */
+            output.Attributes.Remove(new TagHelperAttribute("hello-prefix"));
+            /*
+             * Get the html element content asynchronously
+             */
+            TagHelperContent content = await output.GetChildContentAsync();
+            /*
+             * Prepend "Hello " to the initial content
+             */
+            output.Content.SetHtmlContent("Hello " + content.GetContent());
+        }
+    }
+}
